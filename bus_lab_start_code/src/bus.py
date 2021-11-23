d26217a5d5453eb00292dd4493870f2165ef33e8
Toms Druvaskalns <tomsdruvas@gmail.com>
2021-11-23 13:51:15 +0000

    first commit

---
+++ bus_lab_start_code/src/bus.py
@@ -0,0 +1,10 @@
+class Bus:
+    def __init__(self, route_number, destination, price, capacity):
+        self.route_number = route_number
+        self.capacity = capacity
+        self.destination = destination
+        self.price = price
+    
+    def drive(self):
+        return "Brum brum"
+    
